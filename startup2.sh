42f17dce5be31624b646414aec08724cffc06bb6
Herman <herman@dompseler.nl>
2019-06-19 01:40:34 +0200

    use correct varaiable

--- startup2.sh
+++ startup2.sh
@@ -6,6 +6,6 @@ set -e
 
 # replace variables in default.conf
 NGINX_CONF="/data/conf/default.conf"
-sed -i "s~%PHP_SERVICE%~${PHP_STACK_SERVICE}_php~g" "$NGINX_CONF"
+sed -i "s~%PHP_SERVICE%~${PHP_STACK_SERVICE}~g" "$NGINX_CONF"
 
 exec "$@"
