3c8e72df7295c21a18074020c9aee750c042dca0
Herman <herman@dompseler.nl>
2019-07-04 11:14:12 +0200

    fingerprints foor all environments

--- go.sh
+++ go.sh
@@ -29,14 +29,17 @@ export STACK_PORT=$PORT
 if [ $NETWORK == "dev-net" ]; then
         export STACK_NETWORK_URL=https://portaal.dev.dlo.surf.nl
         export STACK_NETWORK_URL_IDP=https://idp.dev.dlo.surf.nl
+        export STACK_NETWORK_IDP_FINGERPRINT=D9:BD:30:11:E7:1D:12:FA:92:E9:3F:95:D6:C4:24:B5:CD:D3:6F:AF
 fi
 if [ $NETWORK == "test-net" ]; then
         export STACK_NETWORK_URL=https://portaal.test.dlo.surf.nl
         export STACK_NETWORK_URL_IDP=https://idp.test.dlo.surf.nl
+        export STACK_NETWORK_IDP_FINGERPRINT=54:14:99:9B:9F:91:BE:DE:BA:0E:00:87:4A:88:35:49:68:F9:D9:82
 fi
 if [ $NETWORK == "exp-net" ]; then
         export STACK_NETWORK_URL=https://portaal.experimenteer.dlo.surf.nl
         export STACK_NETWORK_URL_IDP=https://idp.experimenteer.dlo.surf.nl
+        export STACK_NETWORK_IDP_FINGERPRINT=D5:42:1D:BB:9F:CE:2F:2E:F7:B5:06:A1:D3:DA:A9:48:D9:3F:21:0B
 fi
 
 # source
