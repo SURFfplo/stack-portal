f6af2d1732c78f43648cd1a5ed14301fed212766
Herman <herman@dompseler.nl>
2019-09-02 11:17:57 +0200

    add API_URL

--- go.sh
+++ go.sh
@@ -29,18 +29,21 @@ export STACK_PORT=$PORT
 if [ $NETWORK == "dev-net" ]; then
         export STACK_NETWORK_URL=https://portaal.dev.dlo.surf.nl
         export STACK_NETWORK_URL_IDP=https://idp.dev.dlo.surf.nl
+        export STACK_NETWORK_URL_API=https://api.dev.dlo.surf.nl
         export STACK_NETWORK_IDP_FINGERPRINT=D9:BD:30:11:E7:1D:12:FA:92:E9:3F:95:D6:C4:24:B5:CD:D3:6F:AF
 	export STACK_ENV_MOD=dev
 fi
 if [ $NETWORK == "test-net" ]; then
         export STACK_NETWORK_URL=https://portaal.test.dlo.surf.nl
         export STACK_NETWORK_URL_IDP=https://idp.test.dlo.surf.nl
+        export STACK_NETWORK_URL_API=https://api.test.dlo.surf.nl
         export STACK_NETWORK_IDP_FINGERPRINT=54:14:99:9B:9F:91:BE:DE:BA:0E:00:87:4A:88:35:49:68:F9:D9:82
 	export STACK_ENV_MOD=prod
 fi
 if [ $NETWORK == "exp-net" ]; then
         export STACK_NETWORK_URL=https://portaal.experimenteer.dlo.surf.nl
         export STACK_NETWORK_URL_IDP=https://idp.experimenteer.dlo.surf.nl
+        export STACK_NETWORK_URL_API=https://api.experimenteer.dlo.surf.nl
         export STACK_NETWORK_IDP_FINGERPRINT=D5:42:1D:BB:9F:CE:2F:2E:F7:B5:06:A1:D3:DA:A9:48:D9:3F:21:0B
 	export STACK_ENV_MOD=prod
 fi
